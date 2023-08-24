88f9fa8397560aa33e93a95126f46b837f4c608e
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-08-24 09:24:06 -0400

    First TODO

--- acquainted.py
+++ acquainted.py
@@ -1,7 +1,7 @@
 """
 Purpose: Get acquainted with basic Python concepts.
 
-Author: Denise Case
+Author: Prabha Sapkota
 
 This file name is:   acquainted.py
 This module name is: acquainted
@@ -48,21 +48,21 @@ logger, logname = setup_logger(__file__)
 # TODO: Customize these by changing the values - use your own name, etc.
 
 # String data types
-name = "John Doe"
-state = "California"
+name = "Prabha Sapkota"
+state = "North Carolina"
 country = "USA"
 
 # Integer data types
-pet_count = 1
-skill_count = 24
+pet_count = 4
+skill_count = 30
 
 # Float data types (floating point numbers, with a decimal point)
-temperature_f = 81.5
-pet_weight_lbs = 12.5
+temperature_f = 78.5
+pet_weight_lbs = 10.5
 
 
 # Boolean data types (True or False)
-has_dog = False
+has_dog = True
 likes_analytics = True
 likes_python = True
 
